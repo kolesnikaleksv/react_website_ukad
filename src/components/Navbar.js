ee24e2e8d8f569da216f62bf637877b66cdc26fa
Alex <kolesnikaleksv@gmail.com>
2021-10-21 12:04:15 +0300

    add links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,40 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../images/UKAD_logo.png';
+import './Navbar.css';
+
 
 function Navbar() {
+const [click, setClick] = useState(false);
+
     return (
         <>
-            <nav className="navbar">
+            <nav className="navbar" background="#022B32" height="78">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        <img
-                            src="/public/UKAD_logo.png"
-                            width="110"
-                            height="27.3"
-                            //className="d-inline-block align-top"
-                            alt="ukadLogo"
-                        />
+                        <img src={logo} alt="Logo" />
                     </Link>
-                    <div className="menu-icon">
-
+                    {/* <div className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <div className='nav-item'>
+                            <Link to='/' className='nav-links' >
+                            Home
+                            </Link>
+                        </div>
+                        <div className='nav-item'>
+                            <Link to='/' className='nav-links' >
+                            Products
+                            </Link>
+                        </div>
+                    </div> */}
+                    <div className='nav-item'>
+                        <Link to='/' className='nav-links active' >
+                        Home
+                        </Link>
+                    </div>
+                    <div className='nav-item'>
+                        <Link to='/' className='nav-links' >
+                        Products
+                        </Link>
                     </div>
                 </div>
 
