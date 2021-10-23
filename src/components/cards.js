477b623807e7877eb66313b727e87b461214f3fc
Alex <kolesnikaleksv@gmail.com>
2021-10-23 09:49:39 +0300

    add Google fonts

--- src/components/cards.js
+++ src/components/cards.js
@@ -5,15 +5,23 @@ import img from '../images/img.png';
 import '@fortawesome/react-fontawesome';
 
 function Cards() {
-
+    fetch('https://api.thedogapi.com/v1/breeds?limit=10&page=0')
+    .then(response => response.json())
+    .then(json =>  Object.entries(json).filter(item => item[0]))
+    .then(arr => console.log(arr));
     return (
         <>
             <div className="cards">
             <div className="title">Product page</div>
                 <div className="cards-container">
-                
-
                     <div className="cards-item">
+                        <img src={img} alt="vegy" />
+                        <div className="menu__item-text">
+                            <div className="menu__item-subtitle">subtitle</div>
+                            <div className="menu__item-descr">Item Descr Adding Additional Styles If you want to use</div>
+                        </div>
+                    </div>
+                    {/* <div className="cards-item">
                         <img src={img} alt="vegy" />
                         <div className="menu__item-text">
                             <div className="menu__item-subtitle">subtitle</div>
@@ -47,14 +55,7 @@ function Cards() {
                             <div className="menu__item-subtitle">subtitle</div>
                             <div className="menu__item-descr">Item Descr Adding Additional Styles If you want to use</div>
                         </div>
-                    </div>
-                    <div className="cards-item">
-                        <img src={img} alt="vegy" />
-                        <div className="menu__item-text">
-                            <div className="menu__item-subtitle">subtitle</div>
-                            <div className="menu__item-descr">Item Descr Adding Additional Styles If you want to use</div>
-                        </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
